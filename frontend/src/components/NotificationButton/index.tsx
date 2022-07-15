460d5918f1251f9a44ad347a253d9955a1833b93
Luan Alves <luan_trabalho12@outlook.com>
2022-07-15 20:13:04 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,19 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../utils/request';
 import './styles.css'
+
+type Props = {
+
+   saleId: number;
+
+}
+
+function handleClick(id:number){
+
+   axios(`${BASE_URL}/sales/${id}/notification`).then(response => {console.log("SUCESSO")})
+
+}
 
 /* o import acima recebeu o nome de icon , mas poderia ser qualquer nome , depois
 de icon vem o from que e para exibir o caminho onde esta determinado arquivo.
@@ -7,11 +21,11 @@ nesse caso o .. ponto ponto / e para indiciar o retorno de um diretorio.
 já o ./indica que esta no mesmo diretório , no caso no diretório atual deste arquivo
 que esta sendo escrito*/
 
-function NotificationButton() {
+function NotificationButton( {saleId} : Props ) {
 
    return (
 
-      <div className="dsmeta-red-btn">
+      <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
 
          <img src={icon} alt="icone" />
 
